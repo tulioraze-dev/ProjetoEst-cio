1c816ebd16d2e4b4a70c80f64f465608d81b942f
razeDev <tuliorazevedo.dev@gmail.com>
2022-06-05 17:34:34 -0300

    ajustando detalhes

--- index.js
+++ index.js
@@ -1,36 +1,35 @@
-
 function imc() {
-
-    let nomeSobrenome = document.querySelector('#nomeSobrenome').value;
-    
+    // Variaveis para calculo do IMC
+    let nomeSobrenome = document.querySelector('#nomeSobrenome').value
+    let peso = parseInt(document.querySelector('#peso').value)
     let altura = parseFloat(document.querySelector('#altura').value)
 
-    let peso = parseFloat(document.querySelector('#peso').value)
-
+    // Calculo do IMC
     let imc = peso / (altura * altura)
 
+    // Define e mostra nivel de IMC de acordo com o resultado do calculo.
     if(imc < 16){
-        return alert(nomeSobrenome + " Seu nivel de IMC é Magreza Grave, Seu IMC é: " + imc.toFixed(2))
+        return alert(nomeSobrenome + " Seu nivel de IMC é: Magreza Grave, Seu IMC é: " + imc.toFixed(2))
     } 
     else if ( imc > 16 && imc < 17) {
-        return alert(nomeSobrenome + " Seu nivel de IMC é Mogreza Moderado, Seu IMC é:  " + imc.toFixed(2))
+        return alert(nomeSobrenome + " Seu nivel de IMC é: Mogreza Moderado, Seu IMC é: " + imc.toFixed(2))
     } 
     else if(imc > 17 && imc < 18.5) {
-        return alert(nomeSobrenome + " Seu nivel de IMC é Magreza leve, Seu IMC é:  " + imc.toFixed(2))
+        return alert(nomeSobrenome + " Seu nivel de IMC é: Magreza leve, Seu IMC é: " + imc.toFixed(2))
     }
     else if (imc > 18.5 && imc < 25) {
-        return alert(nomeSobrenome+ " Seu nivel de IMC é Saudável, Seu IMC é:  " + imc.toFixed(2))
+        return alert(nomeSobrenome+ " Seu nivel de IMC é: Saudável, Seu IMC é: " + imc.toFixed(2))
     }
     else if ( imc > 25 && imc < 30) {
-        return alert(nomeSobrenome + " Seu nivel de IMC é Sobrepeso, Seu IMC é: " + imc.toFixed(2))
+        return alert(nomeSobrenome + " Seu nivel de IMC é: Sobrepeso, Seu IMC é: " + imc.toFixed(2))
     }
     else if (imc > 30 && imc < 35) {
-        return alert(nomeSobrenome + " Seu nível de IMC é Obesidade I, Seu IMC é:" + imc.toFixed(2))
+        return alert(nomeSobrenome + " Seu nível de IMC é: Obesidade I, Seu IMC é: " + imc.toFixed(2))
     }
     else if (imc > 35 && imc < 40) {
-        return alert(nomeSobrenome + " Seu nível de IMC é Obesidade II, Seu IMC é:  " + imc.toFixed(2))
+        return alert(nomeSobrenome + " Seu nível de IMC é: Obesidade II, Seu IMC é: " + imc.toFixed(2))
     }
     else {
-        return alert(nomeSobrenome + " Seu nivel de IMC é Obesidade III, Seu IMC é: " + imc.toFixed(2))
+        return alert(nomeSobrenome + " Seu nivel de IMC é: Obesidade III, Seu IMC é: " + imc.toFixed(2))
     }
 }
